66589326d1031de1b4c99acc3e6be70fc710f32e
shahanasherin-p <shahanasherinp062@gmail.com>
2024-11-17 21:01:20 +0530

    v1

--- index.js
+++ index.js
@@ -7,36 +7,33 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:5173', 
+    origin: 'https://chat-frontend-zeta-eight.vercel.app', 
     methods: ['GET', 'POST']
   }
 });
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://chat-frontend-zeta-eight.vercel.app', 
 }));
 
-// Socket.io connection event
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Handle incoming messages
   socket.on('message', (message) => {
-    io.emit('message', message); // Broadcast message to all connected clients
+    io.emit('message', message); 
   });
 
-  // Handle disconnection
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
 app.get('/',(req,res)=>{
-  res.status(200).send(`<h1 style="color:blue">server started at port and waiting for client request !!!<h1/>`)
+  res.status(200).send(`<h1 style="color:blue">server started at port and waiting for client request<h1/>`)
 })  
